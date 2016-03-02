e1c2d8a82ea1b74aab20dadfc8a9932dc6303a03
Ryan Wechter <ryan.wechter@utexas.edu>
2016-03-02 16:44:38 -0600

    progress so far

--- expand_collapse/subset_expansion.js
+++ expand_collapse/subset_expansion.js
@@ -1,3 +1,60 @@
 $(document).ready(function() {
 
+    //click[0].onclick(tester(0))
+    //click.onclick(tester());
+    $("#click1").onclick(tester());
+
+    function tester() {
+        var click = document.getElementById("click1");
+        var hide = document.getElementById("hide1");
+        if (hide.className == "hide") {
+            hide.className = "";
+            click.textContent = "Show less";
+        } else {
+            hide.className = "hide";
+            click.textContent = "Show more";
+        }
+
+    }
+
+    /*
+     function tester1() {
+     var click = document.getElementById("click2");
+     var hide = document.getElementById("hide2");
+     if (hide.className == "hide") {
+     hide.className = "";
+     click.textContent = "Show less";
+     } else {
+     hide.className = "hide";
+     click.textContent = "Show more";
+     }
+     }
+
+     function tester2() {
+     var click = document.getElementById("click2");
+     var hide = document.getElementById("hide2");
+     if (hide.className == "hide") {
+     hide.className = "";
+     click.textContent = "Show less";
+     } else {
+     hide.className = "hide";
+     click.textContent = "Show more";
+     }
+
+     }
+
+
+     function tester3() {
+     var click = document.getElementById("click3");
+     var hide = document.getElementById("hide3");
+     if (hide.className == "hide") {
+     hide.className = "";
+     click.textContent = "Show less";
+     } else {
+     hide.className = "hide";
+     click.textContent = "Show more";
+     }
+
+     }
+     */
 });
