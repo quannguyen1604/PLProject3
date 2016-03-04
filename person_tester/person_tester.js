c3dbbb60057a2375b020645872ffc719f7d72eca
Ryan Wechter <ryan.wechter@utexas.edu>
2016-03-04 12:37:29 -0600

    Finished part 3

--- person_tester/person_tester.js
+++ person_tester/person_tester.js
@@ -77,117 +77,62 @@ var customer = function(p){
     return f;
 }(person);
 
-var p1 = Object.create(person);
+
 
 
 
 $(document).ready(function() {
 
-    document.writeln("Welcome to the Person Tester application" + "<br><br>");
-    document.writeln("Create customer or employee? (c/e): " + "<input type = text id = 'q1' value = 'c'>");
-
     $("#q1").change(q1);
+    $("#q2s").click(q2);
+
 
     function q1() {
-        //var q1 = document.getElementById('q1')
-
-
-        if ($("#q1").val().toLowerCase() == 'c') {
-            document.writeln("Create customer or employee? (c/e): " + "<input type = text id = 'q1' value = 'c'>");
-            document.write("<br><br>" + "Enter first name: "+"<input type = text id = 'q2a' onchange = 'q2();'>");
-            document.write("<br>" + "Enter last name: "+"<input type = text id = 'q2b' onchange = 'q2();'>");
-            document.write("<br>" + "Enter email address: "+"<input type = text id = 'q2c' onchange = 'q2();'>");
-            document.write("<br>" + "Customer number: "+"<input type = text id = 'q2d' onchange = 'q2();'>");
+        if ($("#q1").val() == "e") {
+            $("#q2t").text("Social security number: ");
+            p1 = Object.create(employee);
         } else {
-            document.writeln("Create customer or employee? (c/e): " + "<input type = text id = 'q1' value = 'e'>");
-            document.write("<br><br>" + "Enter first name: " + "<input type = text id = 'q2a' onchange = 'q2();'>");
-            document.write("<br>" + "Enter last name: " + "<input type = text id = 'q2b' onchange = 'q2();'>");
-            document.write("<br>" + "Enter email address: " + "<input type = text id = 'q2c' onchange = 'q2();'>");
-            document.write("<br>" + "Social security number: " + "<input type = text id = 'q2d' onchange = 'q2();'>");
+            $("#q2t").text("Customer number: ");
+            p1 = Object.create(customer);
+        }
+        if ($("div").attr('class') == "q2") {
+            $("div").toggleClass("q2");
         }
     }
+
+    function q2() {
+        //alert($("#q2a").val());
+        //var p1 = Object.create(customer);
+        p1.run('$firstName')($("#q2a").val());
+        p1.run('$lastName')($("#q2b").val());
+        p1.run('$email')($("#q2c").val());
+
+        if ($("#q1").val() == "e") {
+            p1.run('$ssn')($("#q2d").val());
+        } else {
+            p1.run('$custNo')($("#q2d").val());
+        }
+
+        if ($("#q1").val() == "e") {
+            document.writeln("You entered: <br>")
+            document.writeln("Name: " + p1.run('firstName') + " " + p1.run('lastName') + "<BR>");
+            document.writeln("Email: " + p1.run('email') + "<BR>");
+            document.writeln("Social security number: " + p1.run('ssn') + "<BR>");
+        } else {
+            document.writeln("You entered: <br>")
+            document.writeln("Name: " + p1.run('firstName') + " " + p1.run('lastName') + "<BR>");
+            document.writeln("Email: " + p1.run('email') + "<BR>");
+            document.writeln("Customer number: " + p1.run('custNo') + "<BR>");
+        }
+
+        document.writeln("Continue?" + "<br><input type = button value = 'yes' id = 'yes'>" + "  " + "</t><input type = button value = 'no' id = 'no'>")
+        $("#yes").click(function() {
+            location.reload();
+        });
+        $("#no").click(function() {
+            window.close();
+        });
+    }
+
+
 });
-
-
-
-
-/*
-document.writeln(Object.getPrototypeOf(p1) + "<BR>");
-document.writeln(p1.pname + "<BR>");
-document.writeln(p1.run('name') + "<BR>");
-document.writeln(p1.run('firstName') + "<BR>");
-document.writeln(p1.run('lastName') + "<BR>");
-document.writeln(p1.run('email') + "<BR>");
-p1.run('$firstName')('Amy');
-p1.run('$lastName')('Winehouse');
-p1.run('$email')('amy_winehouse@gmail.com');
-document.writeln(p1.run('firstName') + "<BR>");
-document.writeln(p1.run('lastName') + "<BR>");
-document.writeln(p1.run('email') + "<BR>");
-document.writeln("<BR>");
-
-var e1 = Object.create(employee);
-
-document.writeln(Object.getPrototypeOf(e1) + "<BR>");
-document.writeln(e1.ename + "<BR>");
-document.writeln(e1.run('name') + "<BR>");
-e1.run('$firstName')('Chester');
-e1.run('$lastName')('Bennington');
-e1.run('$email')('chester_benn@gmail.com');
-e1.run('$ssn')('888-66-7777');
-document.writeln(e1.run('firstName') + "<BR>");
-document.writeln(e1.run('lastName') + "<BR>");
-document.writeln(e1.run('email') + "<BR>");
-document.writeln(e1.run('ssn') + "<BR>");
-document.writeln("<BR>");
-
-var c1 = Object.create(customer);
-
-document.writeln(Object.getPrototypeOf(c1) + "<BR>");
-document.writeln(c1.cname + "<BR>");
-document.writeln(c1.run('name') + "<BR>");
-document.writeln(c1.run('firstName') + "<BR>");
-document.writeln(c1.run('lastName') + "<BR>");
-document.writeln(c1.run('email') + "<BR>");
-c1.run('$custNo')('55555');
-document.writeln(c1.run('custNo') + "<BR>");
-document.writeln("<BR>");
-
-
-document.writeln("<BR>" + "Local properties are: <BR>");
-for (var key in Object.getPrototypeOf(p1)) {
-    if (Object.getPrototypeOf(p1).hasOwnProperty(key)) {
-        document.writeln('p1: ' + key + " -> " + Object.getPrototypeOf(p1)[key] + "<BR>");
-    }
-}
-
-document.writeln("<BR>" + "Local properties are: <BR>");
-for (var key in Object.getPrototypeOf(e1)) {
-    if (Object.getPrototypeOf(e1).hasOwnProperty(key)) {
-        document.writeln('e1: ' + key + " -> " + Object.getPrototypeOf(e1)[key] + "<BR>");
-    }
-}
-
-document.writeln("<BR>" + "Local and inherited properties are: <BR>");
-for (var key in Object.getPrototypeOf(e1)) {
-    document.writeln('e1: ' + key + " -> " + Object.getPrototypeOf(e1)[key] + "<BR>");
-}
-
-document.writeln("<BR>" + "Local properties are: <BR>");
-for (var key in Object.getPrototypeOf(c1)) {
-    if (Object.getPrototypeOf(c1).hasOwnProperty(key)) {
-        document.writeln('c1: ' + key + " -> " + Object.getPrototypeOf(c1)[key] + "<BR>");
-    }
-}
-
-document.writeln("<BR>" + "Local and inherited properties are: <BR>");
-for (var key in Object.getPrototypeOf(c1)) {
-    document.writeln('c1: ' + key + " -> " + Object.getPrototypeOf(c1)[key] + "<BR>");
-}
-
-document.writeln("<BR>");
-
-document.writeln("p1 memo is: " + p1.run('memo') + "<BR>");
-document.writeln("e1 memo is: " + e1.run('memo') + "<BR>");
-document.writeln("c1 memo is: " + c1.run('memo') + "<BR>");
-    */
